42ffabfc1e66e0f53cd444fb133bac3207348368
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-11-27 15:38:50 +0900

    Change default locale

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
     config.generators do |g|
