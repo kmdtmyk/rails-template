43c39bbea936a24c46f197c273f35f63106e89ad
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-02-27 10:32:26 +0900

    Add modules for bootstrap

---
+++ app/helpers/bootstrap_helper.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module BootstrapHelper
+
+  def collection_select(*args)
+    set_default_classes(args, 6, %w(form-control))
+    super
+  end
+
+  def date_field(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def datetime_field(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def number_field(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def password_field(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def text_area(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def text_field(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def text_field_tag(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def select(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def select_tag(*args)
+    set_default_classes(args, 2, %w(form-control))
+    super
+  end
+
+  def submit_tag(*args)
+    set_default_classes(args, 1, %w(btn btn-primary))
+    super
+  end
+
+  def set_default_classes(args, index, classes)
+    args[index] ||= {}
+    options = args[index]
+    options[:class] = Array.wrap(options[:class])
+    options[:class].prepend(*classes).uniq!
+  end
+
+end
