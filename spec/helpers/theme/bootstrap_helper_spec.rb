46bb1c564bd4349639f825a9b35194706e2978a0
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-07-03 10:18:07 +0900

    Fix bootstrap helper spec

--- spec/helpers/theme/bootstrap_helper_spec.rb
+++ spec/helpers/theme/bootstrap_helper_spec.rb
@@ -4,67 +4,61 @@ RSpec.describe Theme::BootstrapHelper, type: :helper do
 
   describe 'date_field' do
     it do
-      expect(date_field).to include('form-control')
+      expect(date_field).to include('class="form-control"')
     end
   end
 
   describe 'datetime_field' do
     it do
-      expect(datetime_field).to include('form-control')
+      expect(datetime_field).to include('class="form-control"')
     end
   end
 
   describe 'number_field' do
     it do
-      expect(number_field).to include('form-control')
+      expect(number_field).to include('class="form-control"')
     end
   end
 
   describe 'password_field' do
     it do
-      expect(password_field).to include('form-control')
+      expect(password_field).to include('class="form-control"')
     end
   end
 
   describe 'search_field' do
     it do
-      expect(search_field).to include('form-control')
+      expect(search_field).to include('class="form-control"')
     end
   end
 
   describe 'text_area' do
     it do
-      expect(text_area).to include('form-control')
+      expect(text_area).to include('class="form-control"')
     end
   end
 
   describe 'text_field' do
     it do
-      expect(text_field).to include('form-control')
+      expect(text_field).to include('class="form-control"')
     end
   end
 
   describe 'text_field_tag' do
     it do
-      expect(text_field_tag).to include('form-control')
-    end
-  end
-
-  describe 'select' do
-    it do
-      expect(select).to include('form-control')
+      expect(text_field_tag).to include('class="form-control"')
     end
   end
 
   describe 'select_tag' do
     it do
-      expect(select_tag).to include('form-control')
+      expect(select_tag).to include('class="form-control"')
     end
   end
 
   describe 'submit_tag' do
     it do
-      expect(submit_tag).to include('btn btn-primary')
+      expect(submit_tag).to include('class="btn btn-primary"')
     end
   end
 
