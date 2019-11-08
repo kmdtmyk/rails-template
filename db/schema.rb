94d4b4357329af36ef5bf38e264f9e7ffbb864b4
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-11-08 11:33:03 +0900

    Change migration order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_074029) do
+ActiveRecord::Schema.define(version: 2019_11_08_023142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2019_09_13_074029) do
     t.string "name"
     t.integer "price"
     t.date "release_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
