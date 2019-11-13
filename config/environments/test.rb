3ac1700c96452be68c79650123e27a5005233e63
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-11-13 16:08:21 +0900

    Disable colored logging in test mode

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,4 +45,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # Disable colored logging
+  config.colorize_logging = false
 end
