426887776e21c1385ff9acf8cfa5737c141b4824
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-11-19 18:05:06 +0900

    Change devise routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   devise(
     :database_authenticatable,
-    # :registerable,
+    :registerable,
     # :recoverable,
     :rememberable,
     :validatable,
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
 
   root 'root#index'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
+  devise_for :users, {
+    path: '',
+    path_names: {
+      sign_up: '',
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup',
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+    },
   }
 
   resources :books, except: [:show]
