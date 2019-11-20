df1f51419bf832343d9ff13eb4edf53cc5ee6c6a
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-11-20 15:59:24 +0900

    Remove page query from sort link

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -5,6 +5,7 @@ module UrlHelper
   def sort_url(name)
     query = URI.parse(request.url).query || ''
     hash = URI::decode_www_form(query).to_h.transform_keys(&:to_sym)
+    hash.delete :page
 
     if name.to_s.casecmp? params[:sort]&.to_s
       order = 'asc'.casecmp?(params[:order]&.to_s) ? 'desc' : 'asc'
