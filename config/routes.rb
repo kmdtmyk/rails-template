ddc16281f1cf897fdce058ab00e57d0ca957b1db
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-01-15 15:43:48 +0900

    Add user password change api

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :login, only: [:create]
+      resource :user, only: [], module: :user do
+        resource :password, only: [:update]
+      end
       resources :books, only: [:index, :show, :create, :update]
     end
   end
