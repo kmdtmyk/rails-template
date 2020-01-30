0e4ea42231230c4b602497035277de291807da83
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-01-30 16:36:53 +0900

    Add user serializer

--- app/controllers/api/v1/logins_controller.rb
+++ app/controllers/api/v1/logins_controller.rb
@@ -8,9 +8,14 @@ class Api::V1::LoginsController < Api::BaseController
     user = User.find_by(username: params[:username])
     if user.nil? or !user.valid_password?(params[:password])
       raise ApplicationError::Unauthorized
-      return
     end
-    render json: { user: user, token: user.create_auth_token }
+
+    render json:{
+      user: ActiveModelSerializers::SerializableResource.new(
+        user, serializer: Api::V1::UserSerializer
+      ),
+      token: user.create_auth_token,
+    }
   end
 
 end
---
+++ app/serializers/api/v1/user_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Api::V1::UserSerializer < ActiveModel::Serializer
+
+  attributes(
+    :id,
+    :username,
+    :name,
+    :furigana,
+    :admin,
+  )
+
+end
