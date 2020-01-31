41ab0001db864661ccba0e148b55d149220833d4
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-01-31 10:15:38 +0900

    Use insert_all instead of create

--- db/seeds/notice.rb
+++ db/seeds/notice.rb
@@ -4,7 +4,10 @@ def notice
 
   return if Notice.any?
 
+  notices = []
+
   100.times do
+
     publish_start_datetime = Faker::Time.between(from: 1.year.ago, to: 1.month.after)
 
     if Faker::Boolean.boolean(true_ratio: 0.1)
@@ -15,12 +18,16 @@ def notice
       publish_end_datetime = nil
     end
 
-    Notice.create(
+    notices << {
       title: Faker::Lorem.sentence,
       body: Faker::Lorem.paragraph,
       publish_start_datetime: publish_start_datetime,
       publish_end_datetime: publish_end_datetime,
-    )
+      created_at: Time.current,
+      updated_at: Time.current,
+    }
   end
+
+  Notice.insert_all notices
 
 end
