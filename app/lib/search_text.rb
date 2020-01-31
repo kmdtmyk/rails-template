03f042c7264cc7a8731ed24325104b959620a2c7
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-01-31 11:11:58 +0900

    Add module for search text

---
+++ app/lib/search_text.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module SearchText
+
+  def self.normalize(text)
+    return if text.nil?
+    result = text
+    result = result.gsub('う゛', 'ヴ')
+    result = result.unicode_normalize(:nfkc).downcase
+    result = NKF.nkf('-w --katakana', result)
+  end
+
+end
