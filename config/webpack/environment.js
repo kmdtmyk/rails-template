ceac2b362f77c0eec530b58c750f7e8718d88a75
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-02-03 09:46:31 +0900

    Add webpack config alias

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,4 +8,14 @@ environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('pug', pug)
 
+const path = require('path')
+
+environment.config.merge({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, '../../app/javascript/packs'),
+    },
+  },
+})
+
 module.exports = environment
