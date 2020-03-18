f111c640670b0135fae5a92d62980636d2a0d615
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-03-18 13:52:27 +0900

    Add web api query test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,13 +7,45 @@ RSpec.describe Order, type: :model do
   describe 'WebApi.index' do
 
     example 'number' do
-      expect(Order::WebApi.index(q: 'id:1').to_sql).to start_with Order.where(id: 1).to_sql
-      expect(Order::WebApi.index(q: '-id:1').to_sql).to start_with Order.where.not(id: 1).to_sql
+      expect(Order::WebApi.index(q: 'id:1').to_sql).to start_with(
+        Order.where(id: 1).to_sql
+      )
+      expect(Order::WebApi.index(q: '-id:1').to_sql).to start_with(
+        Order.where.not(id: 1).to_sql
+      )
+    end
+
+    example 'string' do
+      expect(Order::WebApi.index(q: 'comment:"foo"').to_sql).to start_with(
+        Order.where(comment: 'foo').to_sql
+      )
+      expect(Order::WebApi.index(q: '-comment:"foo"').to_sql).to start_with(
+        Order.where.not(comment: 'foo').to_sql
+      )
+    end
+
+    example 'array' do
+      expect(Order::WebApi.index(q: 'id:[1,2,3]').to_sql).to start_with(
+        Order.where(id: [1, 2, 3]).to_sql
+      )
+      expect(Order::WebApi.index(q: '-id:[1,2,3]').to_sql).to start_with(
+        Order.where.not(id: [1, 2, 3]).to_sql
+      )
     end
 
     example 'null' do
-      expect(Order::WebApi.index(q: 'id:null').to_sql).to start_with Order.where(id: nil).to_sql
-      expect(Order::WebApi.index(q: '-id:null').to_sql).to start_with Order.where.not(id: nil).to_sql
+      expect(Order::WebApi.index(q: 'id:null').to_sql).to start_with(
+        Order.where(id: nil).to_sql
+      )
+      expect(Order::WebApi.index(q: '-id:null').to_sql).to start_with(
+        Order.where.not(id: nil).to_sql
+      )
+    end
+
+    example 'multiple' do
+      expect(Order::WebApi.index(q: 'id:1 comment:"foo"').to_sql).to start_with(
+        Order.where(id: 1).where(comment: 'foo').to_sql
+      )
     end
 
   end
