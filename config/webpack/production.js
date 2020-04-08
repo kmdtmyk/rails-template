4edd49c606dfca939925d8656900b86ae4e5eec9
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-04-08 10:22:28 +0900

    Disabled console log on production mode

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+
 module.exports = environment.toWebpackConfig()
