8e83984ed210e24882443c2b25314e94f513c8f1
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-10-22 18:04:35 +0900

    Disabled sourcemap on production mode

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+// sourcemapを無効にする
+environment.config.devtool = false
+
+// console.logを消す
 environment.config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
 
 module.exports = environment.toWebpackConfig()
