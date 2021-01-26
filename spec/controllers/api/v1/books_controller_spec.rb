3848bd176737568ca15a1b4f541fdd0466d054fb
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-01-26 10:25:58 +0900

    Fix controller tests' request body

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -7,34 +7,39 @@ RSpec.describe Api::V1::BooksController, type: :controller do
   describe '#create' do
 
     example 'json' do
-      body = { name: 'book1', price: 100, release_date: '2019-10-15', foo: 123 }.to_json
-      post :create, body: body
+      post :create, body: {
+        name: 'book1',
+        price: 100,
+        release_date: '2019-10-15',
+        foo: 123,
+      }.to_json, as: :json
       expect(response.status).to eq 200
       expect(JSON.parse(response.body)).to be_a Hash
       expect(Book.count).to eq 1
     end
 
     example 'array' do
-      body = [
+      post :create, body: [
         { name: 'book1', price: 100, release_date: '2019-10-15', foo: 123 },
         { name: 'book2', price: 200, release_date: '2019-10-16' },
-      ].to_json
-      post :create, body: body
+      ].to_json, as: :json
       expect(response.status).to eq 200
       expect(JSON.parse(response.body)).to be_a Array
       expect(Book.count).to eq 2
     end
 
     example 'lower camel case' do
-      body = { releaseDate: '2019-10-15' }.to_json
-      post :create, body: body
+      post :create, body: {
+        releaseDate: '2019-10-15'
+      }.to_json, as: :json
       result = JSON.parse(response.body, symbolize_names: true)
       expect(result[:release_date]).to eq '2019-10-15'
     end
 
     example 'upper camel case' do
-      body = { ReleaseDate: '2019-10-15' }.to_json
-      post :create, body: body
+      post :create, body: {
+        ReleaseDate: '2019-10-15'
+      }.to_json, as: :json
       result = JSON.parse(response.body, symbolize_names: true)
       expect(result[:release_date]).to eq '2019-10-15'
     end
@@ -42,17 +47,17 @@ RSpec.describe Api::V1::BooksController, type: :controller do
     describe 'nested attribute' do
 
       example 'json' do
-        body = { reviews: [{ content: 'review1' }] }.to_json
-        post :create, body: body
+        post :create, body: {
+          reviews: [{ content: 'review1' }]
+        }.to_json, as: :json
         expect(BookReview.count).to eq 1
       end
 
       example 'array' do
-        body = [
+        post :create, body: [
           { reviews: [{ content: 'review1' }] },
           { reviews: [{ content: 'review1' }, { content: 'review2' }] },
-        ].to_json
-        post :create, body: body
+        ].to_json, as: :json
         expect(BookReview.count).to eq 3
       end
 
@@ -64,8 +69,13 @@ RSpec.describe Api::V1::BooksController, type: :controller do
 
     example 'update attributes' do
       book = Book.create(name: 'book1', price: 100, release_date: '2019-10-15')
-      body = { id: book.id, name: 'book2', price: 200, release_date: '2019-10-20', foo: 123 }.to_json
-      patch :update, params: { id: book.id }, body: body
+      patch :update, params: { id: book.id }, body: {
+        id: book.id,
+        name: 'book2',
+        price: 200,
+        release_date: '2019-10-20',
+        foo: 123,
+      }.to_json, as: :json
       book.reload
       expect(book.name).to eq 'book2'
       expect(book.price).to eq 200
@@ -82,8 +92,9 @@ RSpec.describe Api::V1::BooksController, type: :controller do
         { id: review2.id, _destroy: true },
         { content: 'new review' },
       ]
-      body = { id: book.id, reviews: reviews }.to_json
-      patch :update, params: { id: book.id }, body: body
+      patch :update, params: { id: book.id }, body: {
+        id: book.id, reviews: reviews
+      }.to_json, as: :json
       book.reload
       review1.reload
       expect(book.reviews.count).to eq 2
