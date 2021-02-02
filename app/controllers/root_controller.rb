20c035818da5ddc6d0075cd2ab35488e11c4b05d
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-02-02 10:33:52 +0900

    Fix indent

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,10 +4,10 @@ class RootController < BaseController
 
   def index
     @notices = Notice
-    .published
-    .order(publish_start_datetime: :desc)
-    .page
-    .per(Notice::PER_PAGE)
+      .published
+      .order(publish_start_datetime: :desc)
+      .page
+      .per(Notice::PER_PAGE)
   end
 
 end
