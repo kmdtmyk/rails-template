23e161b975f03d45da084495b511af2f6e1ed98e
kmdtmyk <winback_banderas@yahoo.co.jp>
2022-07-26 15:46:34 +0900

    Add auto transform keys to broadcast message

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -2,5 +2,15 @@
 
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+
+    def self.broadcast_to(model, message)
+      if message.class == Hash
+        # js側ではlowerCamelCaseで取り扱いたいので自動で変換されるようにする
+        message = message.transform_keys{ _1.to_s.camelize(:lower) }
+      end
+
+      super(model, message)
+    end
+
   end
 end
